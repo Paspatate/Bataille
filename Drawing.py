e05bdd91ef3bf2853af7f6a298711ea0b7cbf59b
Paspatate <82947263+Paspatate@users.noreply.github.com>
2021-12-14 16:47:20 +0100

    update de ui

--- Drawing.py
+++ Drawing.py
@@ -1,6 +1,8 @@
 import pygame
 import main
 
+pygame.init()
+
 clock = pygame.time.Clock()
 
 W = 1280  # variable de la taille de la fenêtre
@@ -18,7 +20,7 @@ class Draw:
         self.dictCard = dict()
 
 
-        for i in ["pique","trefle","coeur","carreau"]:
+        for i in ["♠","♣","♥","♦"]:
             for e in range(2,15):
                 cardsName.append(f"{e}{i}")
 
@@ -28,6 +30,9 @@ class Draw:
 
         for i in range(len(keyCard)):
             self.dictCard[cardsName[i]] = pygame.image.load(f"{path_prefix}/{keyCard[i]}.png").convert_alpha()
+
+        self.defaultFont = pygame.font.Font(pygame.font.get_default_font(), 35)
+
 draw = Draw()
 
 
@@ -36,12 +41,14 @@ n=0
 run = True
 while run:
 
+    screen.fill(color=(25,150,25))
+
     #print(f" post tour len joueur1 {len(self.joueur1)} len joueur2 {len(self.joueur2)}")
     if not len(main.game.joueur1) == 0 and not len(main.game.joueur2) == 0:
         #print(f"len joueur1 {len(self.joueur1)} len joueur2 {len(self.joueur2)}")
         main.game.tour()
-        j1pos = ((W//2)-draw.dictCard["2pique"].get_size()[0]//2, 10)
-        j2pos = ((W//2)-draw.dictCard["2pique"].get_size()[0]//2, H-draw.dictCard["2pique"].get_size()[1]-10)
+        j1pos = ((W//2)-draw.dictCard["2♦"].get_size()[0]//2, 10)
+        j2pos = ((W//2)-draw.dictCard["2♦"].get_size()[0]//2, H-draw.dictCard["2♦"].get_size()[1]-10)
 
         j1played = str(main.game.j1_carte.valeur)+ str(main.game.j1_carte.symbole)
         j2played = str(main.game.j2_carte.valeur)+ str(main.game.j2_carte.symbole)
@@ -55,18 +62,32 @@ while run:
 
 
         n += 1
+        if main.game.state == 2:
+            screen.blit(draw.defaultFont.render("Joueur 2 Gagne",True,(0,0,0)),((W//2)-100,H//2))
+        if main.game.state == 1:
+            screen.blit(draw.defaultFont.render("Joueur 1 Gagne", True, (0,0,0)), ((W//2)-100,H//2))
+
+        screen.blit(draw.defaultFont.render(f"paquet joueur 1 {len(main.game.joueur1)}",True,(0,0,0)),(j1pos[0]-350,j1pos[1]))
+        screen.blit(draw.defaultFont.render(f"paquet joueur 2 {len(main.game.joueur2)}",True,(0,0,0)),(j2pos[0]-350,j2pos[1]))
+        screen.blit(draw.defaultFont.render(f"pile de bataille {len(main.game.pile_centrale)}",True,(0,0,0)),(0,H-30))
+
+
+        #pygame.display.flip()
+
+    if len(main.game.joueur1) == 0 or len(main.game.joueur2) == 0:
+        if main.game.state == 2:
+            screen.blit(draw.defaultFont.render("Joueur 2 Gagne",True,(0,0,0)),((W//2)-100,H//2))
+        if main.game.state == 1:
+            screen.blit(draw.defaultFont.render("Joueur 1 Gagne", True, (0,0,0)), ((W//2)-100,H//2))
+
+        screen.blit(draw.defaultFont.render(f"nbre de tour : {n}", True, (0,0,0)),(W//2,H//2))
+
         pygame.display.flip()
 
-
-
-
-    screen.fill(color=(25,150,25))
     if not run:
         break
-
-
-
-    clock.tick(30)
+    pygame.display.flip()
+    clock.tick(60)
     # teste des events
     for event in pygame.event.get():
         # joueur quitte ?
